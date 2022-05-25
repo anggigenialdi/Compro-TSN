a7f96dfe28db85fbf1d0015e2234cb1618025037
Anggi Genialdi <anggigenialdi@gmail.com>
2022-05-25 19:57:40 +0700

    update home

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -41,11 +41,11 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
+            <a class="sidebar-brand d-flex align-items-center justify-content-center"  href="{{ route('home') }}">
+                {{-- <div class="sidebar-brand-icon rotate-n-15">
+                    
                 </div>
-                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+                <div class="sidebar-brand-text mx-3">CMS</div> --}}
             </a>
 
             <!-- Divider -->
